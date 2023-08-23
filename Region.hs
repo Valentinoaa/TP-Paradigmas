7f94491df841658b503bae624990711dce90a234
Valentino Arbelaiz <valentinoarbelaiz@gmail.com>
2023-08-23 16:31:34 -0300

    Fix

--- Region.hs
+++ Region.hs
@@ -11,7 +11,7 @@ import Tunel
 
 data Region = Reg [City] [Link] [Tunel]
 newR :: Region
-newR = Region -- espera los parametros o se inicializa vacio??? no se deberia inicializar con Reg [] [] []??
+newR = Reg -- espera los parametros o se inicializa vacio??? no se deberia inicializar con Reg [] [] []??
 
 foundR :: Region -> City -> Region -- agrega una nueva ciudad a la región
 foundR (Reg cities _ _) city = newR (city : cities) _ _ -- Hay que checkear si la ciudad ya está en la region (ez)
@@ -20,7 +20,7 @@ linkR :: Region -> City -> City -> Quality -> Region -- enlaza dos ciudades de l
 linkR (Reg _ links _) c1 c2 q = newR _ (newL c1 c2 q : links) _ -- Lo mismo con el link
 
 tunelR :: Region -> [ City ] -> Region -- genera una comunicación entre dos ciudades distintas de la región
-tunelR (Reg cts links tunels) cities = newR cts links (newT (getLinks cities) : tunels) -- Hay que hacer una lista de links y con eso crear el tunel 
+tunelR (Reg _ links tunels) cities = newR _ links (newT (getLinks links cities) : tunels) -- Hay que hacer una lista de links y con eso crear el tunel 
 
 connectedR :: Region -> City -> City -> Bool -- indica si estas dos ciudades estan conectadas por un tunel
 connectedR (Reg _ _ tunels) c1 c2 = foldr (\x acc -> acc || connectsT c1 c2 x) False tunels
@@ -29,16 +29,25 @@ linkedR :: Region -> City -> City -> Bool -- indica si estas dos ciudades estan 
 linkedR (Reg _ links _) c1 c2 = foldr (\x acc -> acc || linksL c1 c2 x) False links
 
 delayR :: Region -> City -> City -> Float -- dadas dos ciudades conectadas, indica la demora
+delayR x y z = 0.1
 
 availableCapacityForR :: Region -> City -> City -> Int -- indica la capacidad disponible entre dos ciudades
+availableCapacityForR x y z = 1
 
-validT :: Region -> [City] -> Bool
-validT (Reg _ links _) [c1, c2] = linkedR links c1 c2 -- Ver como optimizar, atajar casos de listas con <1 ciudad
-validT (Reg _ links _) (x:y:xs) = linkedR links x y && linkedR (y:xs) 
-
--- Mergear la validación de validar el tunel con el getLinks??
-getLinks :: [City] -> [Link]
+-- Esta funcion no es necesaria. (Quizas) Depende de como manejemos construir el tunel
+validT :: [Link] -> [City] -> Bool
+validT links [c1, c2] = linkedR links c1 c2 -- Ver como optimizar, atajar casos de listas con <1 ciudad
+validT links (x:y:xs) = linkedR links x y && validT links (y:xs) -- Esto está mal
 
 
--- Uffff hay que checkear capacidad -> Es un problema pq los links en si no tienen contadores (Idea: usar usesT en cada link o algo parecido)
--- Preguntar a Emilio = Es válido hacer un tunel que ya existe?
+-- 1 : Tratar de juntar las 2 funciones, 2 : que todos las funciones tengan de argumento link me parece horrible, ver como fixear
+findLink :: [Link] -> City -> City -> Link
+findLink  [] c1 c2 = 0 -- Raisear error (No existe el link)
+findLink (x:xs) c1 c2 = if linksL c1 c2 x then x else findLink xs c1 c2 
+
+getLinks :: [Link] -> [City] -> [Link]
+getLinks links [x, y] = [findLink links x y]
+getLinks links (x:y:xs) = findLink links x y : getLinks links (y:xs)
+
+
+-- Uffff hay que checkear capacidad -> Es un problema pq los links en si no tienen contadores (Idea: usar usesT en cada link o algo parecido
