f4906d5b8d4fc8b91afb8322223788aa05984099
Valentino Arbelaiz <valentinoarbelaiz@gmail.com>
2023-08-22 22:56:06 -0300

    Update

--- Region.hs
+++ Region.hs
@@ -15,15 +15,29 @@ newR = Region
 
 foundR :: Region -> City -> Region -- agrega una nueva ciudad a la región
 foundR (Reg cities _ _) city = newR (city : cities) _ _ -- Hay que checkear si la ciudad ya está en la region (ez)
+
 linkR :: Region -> City -> City -> Quality -> Region -- enlaza dos ciudades de la región con un enlace de la calidad indicada
 linkR (Reg _ links _) c1 c2 q = newR _ (newL c1 c2 q : links) _ -- Lo mismo con el link
+
 tunelR :: Region -> [ City ] -> Region -- genera una comunicación entre dos ciudades distintas de la región
-tunelR (Reg _ _ tunels) = 
+tunelR (Reg cts links tunels) cities = -- Hay que hacer una lista de links y con eso crear el tunel 
+
 connectedR :: Region -> City -> City -> Bool -- indica si estas dos ciudades estan conectadas por un tunel
+connectedR (Reg _ _ tunels) c1 c2 = foldr (\x acc -> acc || connectsT c1 c2 x) False tunels
+
 linkedR :: Region -> City -> City -> Bool -- indica si estas dos ciudades estan enlazadas
+linkedR (Reg _ links _) c1 c2 = foldr (\x acc -> acc || linksL c1 c2 x) False links
+
 delayR :: Region -> City -> City -> Float -- dadas dos ciudades conectadas, indica la demora
+
 availableCapacityForR :: Region -> City -> City -> Int -- indica la capacidad disponible entre dos ciudades
+
 validT :: Region -> [City] -> Bool
-validT (Reg cities links _) = 
+validT (Reg _ links _) [c1, c2] = linkedR (Reg _ links _) c1 c2 -- Ver como optimizar, atajar casos de listas con <1 ciudad
+validT (Reg _ links _) (x:y:xs) = linkedR x y && linkedR (y:xs)
+
+
+
+
 -- Uffff hay que checkear capacidad -> Es un problema pq los links en si no tienen contadores (Idea: usar usesT en cada link o algo parecido)
 -- Preguntar a Emilio = Es válido hacer un tunel que ya existe?
