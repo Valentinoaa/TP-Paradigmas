99d2a38cce9e6ccbeb9df2b24f958490de480677
Valentino Arbelaiz <valentinoarbelaiz@gmail.com>
2023-09-10 11:13:12 -0300

    ª

--- TP_2/src/main/java/queue/Queue.java
+++ TP_2/src/main/java/queue/Queue.java
@@ -1,24 +1,35 @@
 package queue;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Queue {
+
+	public List<Object> list = new ArrayList<>();
 
   public boolean isEmpty() {
 		// TODO Auto-generated method stub
-		return true;
+
+		return list.isEmpty();
 	}
 
 	public Queue add( Object  cargo ) {
 		// TODO Auto-generated method stub
+		list.add(cargo);
 		return this;
 	}
 
 	public Object take() {
     // TODO Auto-generated method stub
+		Object first = list.get(0);
+
+		return list.get(0);
 		return null;
 	}
 
 	public Object head() {
 		// TODO Auto-generated method stub
+		list.
     return null;
 	}
 
