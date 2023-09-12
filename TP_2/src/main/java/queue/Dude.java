32a7dc54049a69a5f48c423bb1a0738ea32d1d19
Valentino Arbelaiz <valentinoarbelaiz@gmail.com>
2023-09-11 22:25:10 -0300

    Hey Dude

---
+++ TP_2/src/main/java/queue/Dude.java
@@ -0,0 +1,9 @@
+package queue;
+
+public abstract class Dude {
+    public abstract Dude add();
+    public abstract Dude take();
+    public abstract Dude head();
+
+
+}
---
+++ TP_2/src/main/java/queue/GoodDude.java
@@ -0,0 +1,4 @@
+package queue;
+
+public class GoodDude extends Dude{
+}
